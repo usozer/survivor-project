21bb1cd4937be49388405507080c51455b39c3d3
usozer <uygarsozer2021@u.northwestern.edu>
2021-03-21 23:45:57 -0500

    bootstrapping the best nnet

--- modeling.R
+++ modeling.R
@@ -22,8 +22,8 @@ generateWinner <- function(seasonno, fit, type, ...) {
 
 df_norm$winner <- factor(df_norm$winner)
 
-nodes=1:3
-lambdas=3^(-5:2)
+nodes=(1:3)[1]
+lambdas=(3^(-5:2))[5]
 Nrep <- 1
 K<-40  #K-fold CV on each replicate
 n.models = length(nodes)*length(lambdas) #number of different models to fit
@@ -113,6 +113,63 @@ matrix(c(misclass), nrow=length(nodes),
                        paste(round(lambdas,3))))
 
 
+############################################################
+
+yhat=matrix(0,nrow(df_norm),1)
+
+for (k in 1:40) {
+  train_ind <- filter(df_norm, season != k) %>% pull(sid)
+  train <- df_norm[train_ind,-(1:3)]
+  test_ind <- filter(df_norm, season == k) %>% pull(sid)
+  
+  out <- nnet(winner~., size=3, data=train, linout=F, maxit=1000, decay=0.111, skip=FALSE, trace=FALSE)
+  yhat[test_ind,1] <- generateWinner(k, out, type="raw")
+} #end of k loop
+
+df_norm %>% 
+  mutate(predicted=c(yhat)) %>% 
+  filter(winner == 1 & predicted == 0)
+
+
+#############################################################
+
+# Bootstrapping
+
+no.reps <- 1000
+yhat=matrix(0,105,no.reps)
+
+progress <- 0
+bar <- txtProgressBar(min=0, max=40000, style=3)
+for (k in 1:40) {
+  test_ind <- filter(df_norm, season == k) %>% pull(sid)
+  
+  for (i in 1:no.reps) {
+    seasons <- sample((1:40)[-k], 39, replace=TRUE)
+    
+    train <- data.frame()
+    for (a in seasons) train <- rbind(train, filter(df_norm, season==a))
+    train <- train[,4:11]
+    
+    out <- nnet(winner~., size=1, data=train, linout=F, maxit=1000, decay=1/3, skip=FALSE, trace=FALSE)
+    yhat[test_ind,i] <- generateWinner(k, out, type="raw")
+    
+    progress <- progress+1
+    setTxtProgressBar(bar, progress)
+  }
+  
+} #end of k loop
+
+rowSums(yhat)
+
+df_norm %>% 
+  select(season, castaway, winner) %>% 
+  mutate(prob=rowSums(yhat)/10) %>% 
+  write_csv("finaloutputs.csv")
+
+sum(c==1 & y==1)
+
+
+##############################################################
 
 # 
 # #
@@ -153,8 +210,6 @@ matrix(c(misclass), nrow=length(nodes),
 # winner_nn <- nnet(factor(winner)~., size=4, data=train, maxit=1000)
 # 
 # predict(winner_nn, df_norm)
-
-
 # neural net
 # 0.004     0.012     0.037     0.111     0.333         1         3         9
 # 1 nodes 0.4476190 0.4380952 0.4571429 0.4190476 0.4190476 0.4761905 0.4761905 0.4761905
@@ -169,3 +224,9 @@ matrix(c(misclass), nrow=length(nodes),
 # 2 interactions 0.4285714 0.4761905 0.4857143 0.3809524 0.4285714 0.4761905 0.4380952
 # 3 interactions 0.4666667 0.5238095 0.5142857 0.5333333 0.4761905 0.4761905 0.5238095
 # 4 interactions 0.4761905 0.4095238 0.4857143 0.5142857 0.4380952 0.5333333 0.4571429
+# 
+# 
+# [1] 905  95 822 178  50 950 396 604 352 648  50 950 266 734 615 385 128 872 679 321 800 200 440 560 252 747   1 861  20 119 598 102 300 869
+# [35] 131 912   2  86 222 778 323   3 674  36 864 100   1 541 458 631   9 360 123 388 489 384 182 434  90  85 825 380  16 604 101 185 714 939
+# [69]  61  21 913  66 487  53 460 970  30   0  20 309 671 415 443 142 268 442 290 613 382   5 532 258 210 447  11 542  41 773 186 187 448 365
+# [103] 735 188  77
